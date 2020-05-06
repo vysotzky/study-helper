8cf57c70d054a4bfdfbb0657fe5b4dc52d8acdde
vysotzky <freakdorian@gmail.com>
2020-05-06 07:34:43 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <b-container id="app" class="h-100">
+        <div>
+            <h1 class="text-center mb-3">Learning Assistant</h1>
+            <b-row>
+                <b-col lg="4" class="mb-3">
+                    <b-list-group>
+                        <b-list-group-item>Browse tests</b-list-group-item>
+                        <b-list-group-item>Create a test</b-list-group-item>
+                        <b-list-group-item>Take a test</b-list-group-item>
+                    </b-list-group>
+                </b-col>
+                <b-col lg="8">
+                    <router-view></router-view>
+                </b-col>
+            </b-row>
+        </div>
+    </b-container>
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+    export default {
+        name: 'App',
+    }
 </script>
-
 <style>
-#app {
-  margin-top: 60px;
-}
+    #app {
+        margin-top: 10px;
+    }
 </style>
