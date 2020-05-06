a3297383bb8d010eacaea54c66759d76d143af8d
vysotzky <freakdorian@gmail.com>
2020-05-06 06:31:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// Import firebase
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from "./firebase.config"
+
+firebase.initializeApp(firebaseConfig);
+
+Vue.prototype.$db = firebase.firestore()
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
