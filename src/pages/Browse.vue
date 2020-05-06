5cd081b39e8220006bf487613ea6313c19ec113e
vysotzky <freakdorian@gmail.com>
2020-05-06 09:10:20 +0200

    Update Browse.vue

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <b-table striped hover :items="tests"></b-table>
+        <b-table striped hover :items="tests" :fields="tableFields" :busy="busy">
+            <template v-slot:table-busy>
+                <div class="text-center my-2">
+                    <b-spinner class="align-middle mr-3"></b-spinner>
+                    <strong>Loading tests</strong>
+                </div>
+            </template>
+            <template v-slot:cell(actions)="data">
+                <b-button size="sm" variant="danger" @click="deleteTest(data.item.id)">Delete</b-button>
+            </template>
+        </b-table>
     </div>
 </template>
 
@@ -9,22 +19,44 @@
         name: 'browse',
         data () {
             return {
-                tests: []
+                busy: true,
+                tests: [],
+                tableFields: [
+                    {
+                        key: 'name',
+                        label: 'Test name',
+                        sortable: true
+                    },
+                    {
+                        key: 'actions',
+                        label: 'Actions',
+                        sortable: false
+                    }
+                ]
             }
         },
         methods: {
             getTests () {
-                return this.$tests.get()
+                this.busy = true
+                return this.$tests.get().then(query => {
+                    query.forEach((test) => {
+                        this.tests.push({
+                            id: test.id, name: test.data().name
+                        })
+                    })
+                }).finally(() => this.busy = false)
+            },
+            deleteTest (id) {
+                this.$tests.doc(id).delete().then(() => {
+                    this.$emit('alert', 'Test deleted', 'success')
+                    this.tests = this.tests.filter(test => test.id !== id);
+                }).catch(() => {
+                    this.$emit('alert', 'Failed to delete test', 'danger')
+                });
             }
         },
         mounted () {
-            this.getTests().then(query => {
-                query.forEach( (test) =>  {
-                    this.tests.push({
-                        name: test.data().name
-                    })
-                })
-            })
+            this.getTests()
         }
     }
 </script>
