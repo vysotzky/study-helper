d7846a80f89a3ff8dded09ea0a0c5aa7c1b129a4
vysotzky <freakdorian@gmail.com>
2020-05-06 06:41:21 +0200

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>Hello world</div>
+    <div>
+        Hello world
+        <button type="button" @click="add">Add new question</button>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'home',
+        methods: {
+            add () {
+                this.$questions.add({
+                    question: "1",
+                    answer: "2",
+                })
+            }
+        },
+        mounted () {
+        }
     }
 </script>
